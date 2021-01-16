f6b3950704745b73755de7eaa3fbd0b8d6301488
Emma Forsberg <emma.forsberg@yaelev.se>
2021-01-16 15:54:54 +0100

    First exercise

--- Csharp beginner exercises/Program.cs
+++ Csharp beginner exercises/Program.cs
@@ -10,6 +10,24 @@ namespace Csharp_beginner_exercises
     {
         static void Main(string[] args)
         {
+            numberValidation();
+            Console.Read();
+        }
+
+
+        public static void numberValidation()
+        {
+            Console.WriteLine("Enter a number between 1 and 10");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number >= 1 && number <=10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Unvalid");
+            }
         }
     }
 }
